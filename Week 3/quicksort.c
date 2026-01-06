c5ad4c94f870e106bf4c7658a8ec9994a31ddd77
Harish.R <harishhrjr@gmail.com>
2026-01-06 22:05:30 +0530

    Add files via upload

---
+++ Week 3/quicksort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    int temp;
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+    return i + 1;
+}
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+int main() {
+    int arr[] = {157,110,147,122,111,149,151,141,123,112,117,133};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printArray(arr, n);
+    quickSort(arr, 0, n - 1);
+    printArray(arr, n);
+    return 0;
+}
